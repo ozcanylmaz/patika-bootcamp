f551c8a4d8e47d498a07e12d062dba5930e4083b
YILMAZ <ozcan474@icloud.com>
2025-07-20 21:40:54 +0300

    minimum ve maximum

--- src/week1/MinMax.java
+++ src/week1/MinMax.java
@@ -1,4 +1,34 @@
 package week1;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class MinMax {
+
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("kaç adet sayı gireceksiniz" );
+    int n = input.nextInt();
+
+    int min = 0;
+    int max = 0;
+    int number ;
+
+    for (int i = 1; i <= n; i++) {
+        System.out.println(i + ". sayıyı giriniz : ");
+         number = input.nextInt();
+         4
+        if (number < min) {
+            min = number;
+        }
+        if (number > max){
+            max = number ;
+        }
+    }
+
+    System.out.println("girilen sayılar arasında en küçük sayı : " + min);
+    System.out.println("girilen sayılar arasında en büyük sayı : " + max);
+}
+
 }
