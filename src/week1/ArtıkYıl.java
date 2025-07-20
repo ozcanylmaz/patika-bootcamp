538cef94d43c90dbb810ac234d722051d375a9f6
YILMAZ <ozcan474@icloud.com>
2025-07-20 17:42:36 +0300

    Aritmetik İşlemler ödevi eklendi

---
+++ src/week1/ArtıkYıl.java
@@ -0,0 +1,26 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ArtıkYıl {
+
+        public static void main(String[] args) {
+            // 4'e tam bölünecek
+            // 100'ün katlarından 400'ün katları olanlar sayılacak
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("Yıl giriniz " );
+            int year = input.nextInt();
+
+            // 1700
+            // 2024
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year + "Artık yıldır.");
+            } else {
+                System.out.println(year + "Artık yıl değişir.");
+
+            }
+        }
+    }
+
