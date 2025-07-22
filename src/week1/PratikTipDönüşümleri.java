80825b1025fb4ca1e21784c98ae490730b766351
YILMAZ <ozcan474@icloud.com>
2025-07-22 22:59:30 +0300

    PTD

---
+++ src/week1/PratikTipDönüşümleri.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class PratikTipDönüşümleri {
+
+    public static void main(String[] args) {
+
+        int sayi1 =10;
+        double sayi2 =105.47;
+
+         //sayi2=sayi1;
+
+        //System.out.println(sayi2);
+
+        sayi1=(int)sayi2;
+        System.out.println(sayi1);
+
+        }
+    }
