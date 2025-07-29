112ae1b8a32420e06bf4e05cd81f57821bebf28e
YILMAZ <ozcan474@icloud.com>
2025-07-29 18:09:05 +0300

    maaş hesaplama ödevi eklendi

--- src/week2/MaaşHesaplama/MaaşHesaplama.java
+++ src/week2/MaaşHesaplama/MaaşHesaplama.java
@@ -1,17 +1,11 @@
 package week2.MaaşHesaplama;
 
-import week2.Eployee.Employee;
-
 public class MaaşHesaplama {
     public static void main(String[] args) {
 
-    Employee emp  = new Employee("Yılmaz özcan", 2000,45,1985);
-
-    emp.tax();
-    emp.bonus();
-    emp.raiseSalary();
-    emp.printInfo();
-}
+        Employee emp = new Employee("Yılmaz özcan", 2000, 45, 1985);
+        System.out.println(emp.toString());
+    }
 
 }
 
